c2ef85ea6878f87f5e1ce241a5e0533fa8c123dc
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-06-11 22:26:35 +0300

    name to formControlName

--- src/app/layout/home/staff/insert-staff/insert-staff.component.ts
+++ src/app/layout/home/staff/insert-staff/insert-staff.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-insert-staff',
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './insert-staff.component.css'
 })
 export class InsertStaffComponent {
+  form : FormGroup;
 
+  constructor(fBuilder : FormBuilder) {
+     this.form = fBuilder.group({
+       prenom : [''],
+       nom : [''],
+       username : [''],
+       dtn : [''],
+       mail : [''],
+       mdp : [''],
+       role : [''],
+       post : [''],
+       date_embauche : [''],
+       salaire : ['']
+     })
+   }
+
+   submitForm() {
+    const data ={
+      prenom : this.form.get('prenom')?.value,
+      nom : this.form.get('nom')?.value,
+      username : this.form.get('username')?.value,
+      mail : this.form.get('mail')?.value,
+      mot_de_passe : this.form.get('mdp')?.value,
+      poste : this.form.get('post')?.value,
+      date_embauche : this.form.get('date_embauche')?.value,
+    // mbola tsy misy salaire sy role 
+    };
+   }
 
 }
