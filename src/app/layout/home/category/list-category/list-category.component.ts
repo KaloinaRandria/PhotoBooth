676cb22f9d3e4cee36f49bfe7f830dcdb333f737
MaheryJeremie <mramahay2103@gmail.com>
2024-06-12 14:38:54 +0300

    Integration et liste categorie

--- src/app/layout/home/category/list-category/list-category.component.ts
+++ src/app/layout/home/category/list-category/list-category.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CategoryService} from "../../../../service/category/category.service";
+import {Constants} from "../../../../class/util/constants";
 
 @Component({
   selector: 'app-list-category',
   templateUrl: './list-category.component.html',
-  styleUrl: './list-category.component.css'
+  styleUrls: ['./list-category.component.css']
 })
-export class ListCategoryComponent {
+export class ListCategoryComponent implements OnInit {
+  categories: any[] = [];
 
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories(): void {
+    const url = `/categ/all`;
+    console.log('Fetching categories from:', url);
+    this.categoryService.getAll(url).subscribe(
+      (response: any) => {
+        if (response.success) {
+          this.categories = response.data;
+        } else {
+          console.error('Failed to fetch categories');
+        }
+      },
+      (error) => {
+        console.error('Error fetching categories', error);
+      }
+    );
+  }
 }
