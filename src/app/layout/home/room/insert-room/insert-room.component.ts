231bb0e1e8e7bf158348e3e2a8a1c0c08e149ea9
MaheryJeremie <mramahay2103@gmail.com>
2024-06-12 16:02:28 +0300

    Bouton supprimer salle et  categorie

--- src/app/layout/home/room/insert-room/insert-room.component.ts
+++ src/app/layout/home/room/insert-room/insert-room.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {CategoryService} from "../../../../service/category/category.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {RoomService} from "../../../../service/room/room.service";
+import {Display} from "../../../../class/util/display";
 
 @Component({
   selector: 'app-insert-room',
@@ -6,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrl: './insert-room.component.css'
 })
 export class InsertRoomComponent {
-
+  form : FormGroup;
+  constructor(private roomService: RoomService,private formBilder:FormBuilder,private snackBar : MatSnackBar) {
+    this.form = formBilder.group({
+      numero : ['']
+    })
+  }
+  submitForm(){
+    const data={
+      numero : this.form.get('numero')?.value
+    };
+    this.roomService.save(data,"/salle/save").subscribe({
+      next:()=>{
+        Display.alert(this.snackBar,"Sended successfully","close",3000,"succes-snackbar");
+      },
+      error:(exception)=>{
+        Display.alert(this.snackBar,(exception.error.message),"close",6000);
+      }
+    })
+  }
 }
